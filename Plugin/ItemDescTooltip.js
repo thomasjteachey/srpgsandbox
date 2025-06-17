1960322dd81f4b78a105bd1a13816ffe705f5c67
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2025-06-17 12:40:46 -0400

    Improve tooltip description fallback

--- Plugin/ItemDescTooltip.js
+++ Plugin/ItemDescTooltip.js
@@ -9,7 +9,51 @@
     var _aliasSetInfoItem = ItemInfoWindow.setInfoItem;
     ItemInfoRenderer._currentItemDesc = '';
     ItemInfoWindow.setInfoItem = function(item) {
-        ItemInfoRenderer._currentItemDesc = (item && typeof item.getDescription === 'function') ? item.getDescription() : '';
+        var desc = '';
+
+        if (item) {
+            if (typeof item.getDescription === 'function') {
+                desc = item.getDescription();
+            }
+
+            if (!desc && typeof item.description === 'string') {
+                desc = item.description;
+            }
+
+            if (!desc && typeof item.desc === 'string') {
+                desc = item.desc;
+            }
+
+            if (!desc && item.custom) {
+                if (typeof item.custom.description === 'string') {
+                    desc = item.custom.description;
+                }
+                else if (typeof item.custom.desc === 'string') {
+                    desc = item.custom.desc;
+                }
+
+                if (!desc) {
+                    var lower;
+                    for (var key in item.custom) {
+                        if (!item.custom.hasOwnProperty(key)) {
+                            continue;
+                        }
+                        if (typeof item.custom[key] === 'string') {
+                            lower = key.toLowerCase();
+                            if (lower.indexOf('desc') !== -1 ||
+                                lower.indexOf('description') !== -1 ||
+                                lower.indexOf('info') !== -1 ||
+                                lower.indexOf('text') !== -1) {
+                                desc = item.custom[key];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        ItemInfoRenderer._currentItemDesc = desc;
         _aliasSetInfoItem.call(this, item);
     };
 
